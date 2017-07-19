3c5003060cdcea85f159dba0fed3aa93bfc1e093
hkimca <harrison.kim.ca@gmail.com>
2017-07-19 13:44:08 -0400

    Initial Commit

---
+++ 2w4d3_every.DoItAgain/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  2w4d3_every.DoItAgain
+//
+//  Created by Seantastic31 on 19/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
